66b5f2962ac90ed700c190dbea921c6befaa6191
Abdallah MEDDAH <meddah.abdellah.spcx@gmail.com>
2024-02-23 16:04:43 +0100

    ✨ add effects

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import van from "vanjs-core";
 const toXActionFn = (name) => (action) => `[${name}] ${action}`;
 export default function vanrx(name, initialState = {}) {
-  const state = van.state(initialState);
-  const toXAction = toXActionFn(name);
-  let actions = {};
-
+  const state = van.state(initialState),
+    toXAction = toXActionFn(name);
+  let actions = {},
+    effects = {};
+  const dispatch = (action, params) => {
+    state.val = (actions[toXAction(action)] || []).reduce(
+      (accState, reducer) => reducer(accState, params),
+      state.val,
+    );
+    (effects[toXAction(action)] || []).forEach(({ effect, dispose }) => {
+      dispose();
+      effect(dispatch);
+    });
+  };
   return [
     (action, reduce) => {
       actions = {
@@ -12,13 +22,16 @@ export default function vanrx(name, initialState = {}) {
         [toXAction(action)]: [...(actions[toXAction(action)] || []), reduce],
       };
     },
-    (action, params) => {
-      state.val = (actions[toXAction(action)] || []).reduce(
-        (accState, reducer) => reducer(accState, params),
-        state.val,
-      );
-    },
+    dispatch,
     (selector) => van.derive(() => selector(state.val)),
+    (action, effectFn) => {
+      const xAction = toXAction(action);
+      const { effect, dispose } = effectFn();
+      effects = {
+        ...effects,
+        [xAction]: [...(effects[xAction] || []), { effect, dispose }],
+      };
+    },
     name,
   ];
 }
